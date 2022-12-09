b4fdfdd20eac0acf8f830ee87572bb5820365a65
ABbdulM <Abdulbinmufid@gmail.com>
2022-12-08 17:52:52 -0800

    task four

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints a given phrase
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzze")
+	return (0);
+}
