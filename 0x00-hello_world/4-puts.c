ab9f7a41af5cb48d0d30c84fe3a1d48c11af6e13
ABbdulM <Abdulbinmufid@gmail.com>
2022-12-11 05:45:40 -0800

    task four

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,13 @@
 
 /**
  * main - prints a given phrase
+ *
  * Return: 0 if exited properly, non-zero otherwise
  */
 
 int  main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 return (0);
 
