65706d20796069371a322d9b10141a02217e5b6f
ABbdulM <Abdulbinmufid@gmail.com>
2022-12-11 06:19:37 -0800

    task six

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,6 +12,7 @@ int main(void)
 	printf("size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("size of a long long int: %d byte(s)\n", sizeof(long ong int));
 	printf("size of a float: %d byte(s)\n", sizeof(float));
-        return (0);
+return (0);
+
 }
 
