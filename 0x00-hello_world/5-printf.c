e699bf6af48c14686a29a0f77d6547559f5d74d1
ABbdulM <Abdulbinmufid@gmail.com>
2022-12-12 02:01:43 -0800

    task five

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,11 @@
 
 /**
  * main - prints the phrase with proper grammer,
- * but the outcone is a piece of art,
+ * but the outcome is a piece of art,
  * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
 	printf("with proper grammer, but the outcome is a piece of art,\n");
 return (0);
-
 }
