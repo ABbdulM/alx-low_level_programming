7a1d26681311183917936d0951ee23283af39a90
ABbdulM <Abdulbinmufid@gmail.com>
2022-12-16 07:12:16 -0800

    task 3

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -2,11 +2,12 @@
 
 /**
  * largest_number - returns the largest of 3 numbers
- * @a: firist integer 
+ * @a: first integer 
  * @b: second integer
  * @c: third integer
  * Return: largest number
  */
+
 int largest_number(int a, int b, int c)
 {
 int largest;
